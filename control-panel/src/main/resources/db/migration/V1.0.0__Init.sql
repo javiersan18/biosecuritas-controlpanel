75e5f7104abdaea6a63b8686c97a54f689aebd2f
Javier Sánchez <jsg_sanchez@hotmail.com>
2019-06-21 11:33:22 +1000

    REFACTOR - NOR WORKING: Changed SQL queries flyways

--- control-panel/src/main/resources/db/migration/V1.0.0__Init.sql
+++ control-panel/src/main/resources/db/migration/V1.0.0__Init.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `user` (
   id IDENTITY NOT NULL PRIMARY KEY auto_increment,
-  username  VARCHAR(50) NOT NULL,
-  password  VARCHAR(255) NOT NULL,
-  email  VARCHAR(255) NOT NULL,
-  date_created TIMESTAMP
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  surname1 VARCHAR(255) NOT NULL,
+  surname2 VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  creation_date TIMESTAMP
 );
 
 CREATE TABLE `authority` (
